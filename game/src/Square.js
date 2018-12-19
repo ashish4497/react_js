d96425dee42ab6aa68d2d197a9f1de47859b00cd
ashish4497 <ak8393267@gmail.com>
2018-12-20 00:01:47 +0530

    tictacgame

---
+++ game/src/Square.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class Square extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          value: null,
+        };
+      }
+    
+      render() {
+        return (
+          <button className="square" onClick={() => alert('click')}>
+            {this.props.value}
+          </button>
+        );
+      }
+  }
+
+  export default Square;
