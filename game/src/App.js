105870abce95d65e4df13bec4bd3e62936c461cc
ashish4497 <ak8393267@gmail.com>
2018-12-20 12:22:19 +0530

    tic tac toe

--- game/src/App.js
+++ game/src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
-import Board from './Board';
+import Game from './Game';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Board />
+        <Game/>
       </div>
     );
   }
--- game/src/Board.js
+++ game/src/Board.js
@@ -1,12 +1,62 @@
 import React, { Component, Fragment } from 'react';
 import Square from './Square';
+
+
 export default class Board extends Component {
+  constructor(props) {
+    super(props)
+    this.state ={
+      squares:Array(9).fill(null),
+      xIsNext: true
+    }
+  }
+
+  handleClick (i) {
+    const squares =this.state.squares.slice();
+    if( this.calculateWinner(squares)|| squares[i]){
+      return;
+    }
+    squares[i]=this.state.xIsNext? 'x': 'o';
+    this.setState({squares:squares,
+    xIsNext:!this.state.xIsNext,
+    })
+  }
+
   renderSquare(i) {
-    return <Square value={i} />;
+    return <Square value={this.state.squares[i]}
+    onClick={() => this.handleClick(i)}
+    />;
+  
+  }
+  
+  calculateWinner(squares) {
+    const lines = [
+      [0,1,2],
+      [3,4,5],
+      [6,7,8],
+      [0,4,8],
+      [2,4,6],
+      [0,3,6],
+      [1,4,7],
+      [2,5,8]
+    ];
+    for(var i =0; i<lines.length; i++){
+      const[a,b,c]= lines[i];
+      if (squares[a] && squares[a] == squares[b] && squares[a] == squares[c]){
+        return squares[a];
+      }
+    }
+    return null;
   }
   render() {
-    const status = 'Next player: X';
-    return (
+    const winner =  this.calculateWinner(this.state.squares);
+    let status;
+    if(winner) {
+      status = 'winner:'+ winner
+    } else { 
+      status = 'Next player:' + (this.state.xIsNext ? 'x' :'o');
+    }
+    return (  
       <Fragment>
         <div className="status">{status}</div>
         <div className="board-row">
