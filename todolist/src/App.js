fe0f96e85cc8ef7e42fe64820cca358acd0d3bff
ashish4497 <ak8393267@gmail.com>
2019-06-02 10:04:17 +0530

    minor changes

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -1,43 +1,42 @@
-import React, { Component } from 'react';
-import {Todo, List } from './Todo';
+import React, { Component } from "react";
+import { Todo, List } from "./Todo";
 
 class App extends Component {
-  constructor(){
-    super()
-    this.state = {
-    items:[]
-    }
-    console.log(this.state.items)
-  } 
+	constructor() {
+		super();
+		this.state = {
+			items: []
+		};
+		console.log(this.state.items);
+	}
 
-  additems = (e) => {
-    e.preventDefault();
-    var textBox = document.querySelector(".text-box");
-    var currentitem = textBox.value;
-    // console.log(currentitem)
-    var newArray = this.state.items.concat(currentitem);
-    this.setState ({
-    items: newArray
-    }
-    )
-   
-    console.log(this.state.items);
-    textBox.value = " ";
-  }
-  toggle = (e) =>{
-    let id = e.target.dataset.id
-    console.log(id)
-  }
+	additems = (e) => {
+		e.preventDefault();
+		var textBox = document.querySelector(".text-box");
+		var currentitem = textBox.value;
+		// console.log(currentitem)
+		var newArray = this.state.items.concat(currentitem);
+		this.setState({
+			items: newArray
+		});
 
-  render() {
-    return (
-      <div className="App">
-        <Todo additems={this.additems} />
-        {
-        this.state.items.map((litem,i)=><List itemList={ litem } toggle={this.toggle} dataId={i} />)
-        }
-      </div>
-    );
-  }
+		console.log(this.state.items);
+		textBox.value = " ";
+	};
+	toggle = (e) => {
+		let id = e.target.dataset.id;
+		console.log(id);
+	};
+
+	render() {
+		return (
+			<div className='App'>
+				<Todo additems={this.additems} />
+				{this.state.items.map((litem, i) => (
+					<List itemList={litem} toggle={this.toggle} dataId={i} />
+				))}
+			</div>
+		);
+	}
 }
 export default App;
